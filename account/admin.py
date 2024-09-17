daba308c47b71842dc1ced79fb57946b859e8e9a
mhdi.gh01 <mehdi.ghash@mtnirancell.ir>
2024-09-18 01:57:05 +0330

    feat: :art: admin file added to customize admin template

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models.user import User
+from .models.role import Role
+from .models.perm import Perm
+from .models.role_perm import RolePerm
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "mobile",
+        "first_name",
+        "last_name"
+    ]
+
+
+@admin.register(Role)
+class RoleAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name"
+    ]
+
+
+@admin.register(Perm)
+class PermAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "name",
+        "lang",
+        "perm_model",
+        "action",
+        "field"
+    ]
+
+
+@admin.register(RolePerm)
+class RolePermAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "role",
+        "perm",
+        "value"
+    ]
