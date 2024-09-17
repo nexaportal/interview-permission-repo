9b0e4fbf4d5ae0e01a2b4be5d2e7a68ce2101914
mhdi.gh01 <mehdi.ghash@mtnirancell.ir>
2024-09-18 01:51:11 +0330

    feat: :art: Perm Serializer , models added

--- account/models/perm.py
+++ account/models/perm.py
@@ -12,16 +12,39 @@ class PermissionActionChoices(models.TextChoices):
 
 
 class Perm(models.Model):
-    name = models.CharField(verbose_name=_("Name"), max_length=150, unique=True)
-    codename = models.CharField(_("codename"), max_length=100)
+    name = models.CharField(
+        verbose_name=_("Name"),
+        max_length=150,
+        unique=True
+    )
+    lang = models.ForeignKey(
+        "content.Language",
+        on_delete=models.DO_NOTHING,
+        verbose_name=_("language"),
+        related_name="lang_perm_items"
+    )
 
-    perm_model = models.ForeignKey(ContentType, models.CASCADE, verbose_name=_("model"), related_name="perm_model")
-    action = models.CharField(choices=PermissionActionChoices.choices, verbose_name=_("action"), max_length=100)
-    field = models.CharField(_("field"), max_length=100, blank=True, null=True)
+    perm_model = models.ForeignKey(
+        ContentType,
+        models.CASCADE,
+        verbose_name=_("model"),
+        related_name="perm_model"
+    )
+    action = models.CharField(
+        choices=PermissionActionChoices.choices,
+        verbose_name=_("action"),
+        max_length=100
+    )
+    field = models.CharField(
+        _("field"),
+        max_length=100,
+        blank=True,
+        null=True
+    )
 
     class Meta:
-        verbose_name = "Role"
-        verbose_name_plural = "Roles"
+        verbose_name = "Perm"
+        verbose_name_plural = "Perms"
         ordering = ["name"]
 
     def __str__(self):
