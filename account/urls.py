2b825be5dcb4ef8131c08997a23f9c04aca2219d
mhdi.gh01 <mehdi.ghash@mtnirancell.ir>
2024-09-18 01:55:02 +0330

    feat: :art: Urls added for apps

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,13 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views.user import MyObtainTokenPairView, RegisterView
+from .views.role import RoleViewSet
 
-from django.urls import path
-from .views import MyObtainTokenPairView, RegisterView
-
+router = DefaultRouter()
+router.register(r"role", RoleViewSet, basename="role")
 
 urlpatterns = [
-    path('auth/login/', MyObtainTokenPairView.as_view(), name='token_obtain_pair'),
-    path('auth/register/', RegisterView.as_view(), name='auth_register'),
+    path("auth/login/", MyObtainTokenPairView.as_view(), name="token_obtain_pair"),
+    path("auth/register/", RegisterView.as_view(), name="auth_register"),
+    path("", include(router.urls)),
 ]
-
--- content/urls.py
+++ content/urls.py
@@ -1,11 +1,12 @@
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet
+from .views.category import CategoryViewSet
+from .views.post import PostViewSet
 
 router = DefaultRouter()
-router.register(r'category', CategoryViewSet)
+router.register(r"category", CategoryViewSet)
+router.register(r"post", PostViewSet)
 
 urlpatterns = [
-    path('api/v1/', include(router.urls)),
+    path("api/v1/", include(router.urls)),
 ]
