2d43eb4efd598c0853d3b69f8136a3d2bb775a19
bahamin <abbasi.bahamin@gmail.com>
2024-09-20 17:54:48 +0330

    permission controller tests finished

--- account/tests/test_permission_controller.py
+++ account/tests/test_permission_controller.py
@@ -66,6 +66,7 @@ class CheckUserPermissionForObjectTestCase(TestCase):
         # Create Roles
         self.role_editor = Role.objects.create(name='Editor')
         self.role_viewer = Role.objects.create(name='Viewer')
+        self.role_other = Role.objects.create(name='OtherRole')
 
         # Create Permissions
         self.perm_update_en = Perm.objects.create(
@@ -92,6 +93,15 @@ class CheckUserPermissionForObjectTestCase(TestCase):
             lang=self.language_en
         )
 
+        # Permissions not related to the PostItem model or English language
+        self.perm_unrelated = Perm.objects.create(
+            name='Unrelated Permission',
+            codename='unrelated_perm',
+            perm_model=self.content_type_postitem,  # Or another ContentType
+            action=PermissionActionChoices.CREATE,
+            lang=self.language_fr  # Different language
+        )
+
         # Create RolePerms
         self.role_perm_editor_update = RolePerm.objects.create(role=self.role_editor, perm=self.perm_update_en)
         self.role_perm_editor_delete = RolePerm.objects.create(role=self.role_editor, perm=self.perm_delete_en)
@@ -100,6 +110,10 @@ class CheckUserPermissionForObjectTestCase(TestCase):
         # Assign RolePerms to Users via User's ManyToManyField role_perms
         self.user1.role_perms.add(self.role_perm_editor_update, self.role_perm_editor_delete)
         self.user2.role_perms.add(self.role_perm_viewer_retrieve)
+
+        # Assign unrelated permissions to user2
+        self.role_perm_other_unrelated = RolePerm.objects.create(role=self.role_other, perm=self.perm_unrelated)
+        self.user2.role_perms.add(self.role_perm_other_unrelated)
 
         # Create Post instances
         self.post_en = Post.objects.create()
@@ -229,3 +243,229 @@ class CheckUserPermissionForObjectTestCase(TestCase):
         # User1 lacks update permission for French language
         result = check_user_permission_for_object(self.user1, self.post_item_fr, mapped_action, action)
         self.assertFalse(result)
+
+    def test_user_with_multiple_roleperms_but_missing_needed_permission(self):
+        """
+        User has multiple role_perms, but not the one needed for the object and action.
+        """
+        mapped_action = 'update'
+        action = 'update'
+
+        # user2 has retrieve permission and an unrelated permission, but lacks update permission
+        result = check_user_permission_for_object(self.user2, self.post_item, mapped_action, action)
+        self.assertFalse(result)  # Should be False because user2 lacks update permission
+
+    def test_permission_assigned_then_removed(self):
+        """
+        A permission is assigned to the user, and permission is checked correctly.
+        Then the permission is removed from the user, and again the permission is checked correctly.
+        """
+        mapped_action = 'update'
+        action = 'update'
+
+        # Initially, user2 cannot update any post_item
+        result_before = check_user_permission_for_object(self.user2, self.post_item, mapped_action, action)
+        self.assertFalse(result_before)
+
+        # Assign update permission to user2
+        perm_update_en_user2 = Perm.objects.create(
+            name='Update English Posts for User2',
+            codename='update_en_posts_user2',
+            perm_model=self.content_type_postitem,
+            action=PermissionActionChoices.UPDATE,
+            lang=self.language_en
+        )
+        role_perm_update_user2 = RolePerm.objects.create(role=self.role_viewer, perm=perm_update_en_user2)
+        self.user2.role_perms.add(role_perm_update_user2)
+
+        # Create a new Post for user2
+        post_user2 = Post.objects.create()
+
+        # Create a PostItem authored by user2 with a unique post and lang combination
+        post_item_user2 = PostItem.objects.create(
+            post=post_user2,
+            author=self.user2,
+            lang=self.language_en,
+            title='User2 Test Post',
+            content='This is a test post by user2.'
+        )
+
+        # Now user2 should be able to update their own post
+        result_after_assignment = check_user_permission_for_object(self.user2, post_item_user2, mapped_action, action)
+        self.assertTrue(result_after_assignment)
+
+        # Remove the update permission from user2
+        self.user2.role_perms.remove(role_perm_update_user2)
+
+        # Now user2 should not be able to update their own post
+        result_after_removal = check_user_permission_for_object(self.user2, post_item_user2, mapped_action, action)
+        self.assertFalse(result_after_removal)
+
+    def test_object_language_changed_user_loses_permission(self):
+        """
+        Test that if an object's language is changed to one the user lacks permission for,
+        the user can no longer perform the action.
+        """
+        # User1 has update permission for English
+        mapped_action = 'update'
+        action = 'update'
+
+        # Ensure user1 can update the post_item initially
+        can_update_before = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertTrue(can_update_before)
+
+        # Change the language of post_item to French (user1 lacks update permission for French)
+        self.post_item.lang = self.language_fr
+        self.post_item.save()
+
+        # Now, user1 should not be able to update the post_item
+        can_update_after = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertFalse(can_update_after)
+
+    def test_object_author_changed_user_loses_ownership(self):
+        """
+        Test that if an object's author is changed, the original author can no longer perform actions requiring ownership.
+        """
+        mapped_action = 'update'
+        action = 'update'
+
+        # Ensure user1 can update the post_item initially
+        can_update_before = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertTrue(can_update_before)
+
+        # Change the author of post_item to user2
+        self.post_item.author = self.user2
+        self.post_item.save()
+
+        # Now, user1 should not be able to update the post_item
+        can_update_after = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertFalse(can_update_after)
+
+    def test_permission_deleted_user_loses_access(self):
+        """
+        Test that if a permission is deleted, the user can no longer perform the associated action.
+        """
+        mapped_action = 'update'
+        action = 'update'
+
+        # Ensure user1 can update the post_item initially
+        can_update_before = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertTrue(can_update_before)
+
+        # Delete the 'update' permission
+        self.perm_update_en.delete()
+
+        # Now, user1 should not be able to update the post_item
+        can_update_after = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertFalse(can_update_after)
+
+    def test_permission_action_modified_user_access_updated(self):
+        """
+        Test that if a permission's action is modified, the user's access is updated accordingly.
+        """
+        # Initially, user1 has 'update' permission
+        mapped_action_update = 'update'
+        action_update = 'update'
+
+        # Ensure user1 can update the post_item
+        can_update_before = check_user_permission_for_object(self.user1, self.post_item, mapped_action_update, action_update)
+        self.assertTrue(can_update_before)
+
+        # Change the permission's action from 'update' to 'delete'
+        self.perm_update_en.action = PermissionActionChoices.DELETE
+        self.perm_update_en.save()
+
+        # Now, user1 should not be able to update the post_item
+        can_update_after = check_user_permission_for_object(self.user1, self.post_item, mapped_action_update, action_update)
+        self.assertFalse(can_update_after)
+
+        # But user1 should be able to delete the post_item
+        mapped_action_delete = 'delete'
+        action_delete = 'destroy'
+        can_delete = check_user_permission_for_object(self.user1, self.post_item, mapped_action_delete, action_delete)
+        self.assertTrue(can_delete)
+
+    def test_role_deleted_user_loses_permissions(self):
+        """
+        Test that if a role is deleted, the user loses the permissions associated with that role.
+        """
+        mapped_action = 'update'
+        action = 'update'
+
+        # Ensure user1 can update the post_item initially
+        can_update_before = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertTrue(can_update_before)
+
+        # Delete the role 'Editor' (which user1 has)
+        self.role_editor.delete()
+
+        # Now, user1 should not be able to update the post_item
+        can_update_after = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertFalse(can_update_after)
+
+    def test_concurrent_modification_of_permissions(self):
+        """
+        Simulate concurrent modification where a user's permission is changed during a permission check.
+        """
+        mapped_action = 'update'
+        action = 'update'
+
+        # Ensure user1 can update the post_item initially
+        can_update_before = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertTrue(can_update_before)
+
+        # Simulate another process removing user1's permission
+        self.user1.role_perms.remove(self.role_perm_editor_update)
+
+        # Now, user1 should not be able to update the post_item
+        can_update_after = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertFalse(can_update_after)
+
+    def test_permission_check_performance_with_many_permissions(self):
+        """
+        Test the performance of the permission check when the user has many permissions.
+        """
+        mapped_action = 'update'
+        action = 'update'
+
+        # Add a large number of irrelevant permissions to user1
+        for i in range(1000):
+            perm = Perm.objects.create(
+                name=f'Irrelevant Permission {i}',
+                codename=f'irrelevant_perm_{i}',
+                perm_model=self.content_type_postitem,
+                action=PermissionActionChoices.CREATE,
+                lang=self.language_en
+            )
+            role_perm = RolePerm.objects.create(role=self.role_editor, perm=perm)
+            self.user1.role_perms.add(role_perm)
+
+        # Ensure that the permission check still functions correctly
+        can_update = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertTrue(can_update)
+
+    def test_permission_checks_with_caching(self):
+        """
+        Test that caching mechanisms do not interfere with permission checks.
+        """
+        from django.core.cache import cache
+
+        mapped_action = 'update'
+        action = 'update'
+
+        # Cache the user's permissions
+        cache_key = f'user_permissions_{self.user1.id}'
+        cache.set(cache_key, list(self.user1.role_perms.all()), timeout=60)
+
+        # Ensure user1 can update the post_item initially
+        can_update_before = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertTrue(can_update_before)
+
+        # Remove the update permission from user1
+        self.user1.role_perms.remove(self.role_perm_editor_update)
+        # Update the cache to simulate stale data
+        cache.set(cache_key, list(self.user1.role_perms.all()), timeout=60)
+
+        # Ensure that the permission check does not use stale cache data
+        can_update_after = check_user_permission_for_object(self.user1, self.post_item, mapped_action, action)
+        self.assertFalse(can_update_after)
